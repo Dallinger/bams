4aa4e9cebe8a8ad3c5b384af690d66337f2783ae
Jordan Suchow <suchow@post.harvard.edu>
2018-01-11 13:20:21 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,52 @@
+"""Test script for BAMS port."""
+
+import numpy as np
+
+import bams
+
+np.random.seed(5822646)
+
+
+def f1(x):
+    """A test oracle function."""
+    return np.sin(x) + 0.2 * np.random.randn()
+
+
+def f2(x):
+    """Another test oracle function."""
+    return x
+
+
+def f3(x):
+    """A third test oracle function."""
+    return max(0, min(0.25 + 0.75 * x**3, 1))
+
+
+learner = bams.Learner(
+    kernels=[
+        "LIN",
+        "PER",
+        "SE",
+        "RQ",
+        "M32",
+        "M52",
+        "E",
+        "ES2",
+        "K",
+        "DP",
+        "LG",
+        "POLY",
+    ],
+    depth=2,
+    query_strategy="random",
+)
+
+for i in range(500):
+    x = learner.query()
+    y = f1(x)
+    learner.train(x, y)
+
+y_predict = np.linspace(0, 1, 50)
+print(learner.predict(y_predict))
+learner.plot_predictions(y_predict)
+print(learner.map_model_kernel)
