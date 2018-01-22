36813807217e312c87b516a5181ba6f83294ef45
Gustavo Malkomes <gustavo.muniz@gmail.com>
2018-01-22 11:24:13 -0600

    first glance

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-import bams
+# import bams
 
 np.random.seed(5822646)
 
@@ -22,31 +22,38 @@ def f3(x):
     return max(0, min(0.25 + 0.75 * x**3, 1))
 
 
-learner = bams.Learner(
-    kernels=[
-        "LIN",
-        "PER",
-        "SE",
-        "RQ",
-        "M32",
-        "M52",
-        "E",
-        "ES2",
-        "K",
-        "DP",
-        "LG",
-        "POLY",
-    ],
-    depth=2,
-    query_strategy="random",
-)
+# learner = bams.Learner(
+#     kernels=[
+#         "LIN",
+#         "PER",
+#         "SE",
+#         "RQ",
+#         "M32",
+#         "M52",
+#         "E",
+#         "ES2",
+#         "K",
+#         "DP",
+#         "LG",
+#         "POLY",
+#     ],
+#     depth=2,
+#     query_strategy="random",
+# )
 
-for i in range(500):
-    x = learner.query()
-    y = f1(x)
-    learner.train(x, y)
+# # for i in range(500):
+# #     x = learner.query()
+# #     y = f1(x)
+# #     learner.train(x, y)
 
-y_predict = np.linspace(0, 1, 50)
-print(learner.predict(y_predict))
-learner.plot_predictions(y_predict)
-print(learner.map_model_kernel)
+# # y_predict = np.linspace(0, 1, 50)
+# # print(learner.predict(y_predict))
+# # learner.plot_predictions(y_predict)
+# # print(learner.map_model_kernel)
+
+def my_loop(generator):
+    for i in generator:
+        print(i)
+
+
+my_loop(range(10))
